7842605443846d3caef1722818d857ef477d81eb
lalovazquez09-sketch <lalovazquez09@gmail.com>
2026-01-11 17:38:56 -0600

    Update lalovazquez09.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/lalovazquez09.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/lalovazquez09.py
@@ -1,1 +1,27 @@
+#https://python.org
 
+# comentario en una linea
+
+"""
+esto tambien es
+un comentario
+en varias lineas
+"""
+
+my_variable = "mi variable"
+my:variable = "nuevo valor de mi variable"
+
+MY_CONSTANT = "mi constante" #por convención
+
+my_int = 1
+my_float = 1.2
+my_bool = True 
+my_bool = False
+my_string = "cadena de texto"
+
+print ("hola,python")
+
+print (type(my_int = 1))
+print (type(my_float = 1.2))
+print (type(my_bool = True))
+print (type(my_bool = False))
