701c3e3c7af882583f3ca6bed1ba312eda50aeb5
lalovazquez09-sketch <lalovazquez09@gmail.com>
2026-01-16 22:21:03 -0600

    lalovazquez09.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/lalovazquez09.py
@@ -0,0 +1,42 @@
+"""
+ operadores
+"""
+
+# operadores aritmeticos
+print (f"Suma: 10 + 3 =  {10 + 3}")
+print (f"Resta: 10 - 3 =  {10 - 3}")
+print (f"Multiplicación: 10 * 3 =  {10 * 3}")
+print (f"Division: 10 / 3 =  {10 / 3}")
+print (f"Modulo: 10 % 3 =  {10 % 3}")
+print (f"Exponente: 10 ** 3 =  {10 ** 3}")
+print (f"Division entera: 10 // 3 =  {10 // 3}")
+
+#OPERADORES DE COMPARACIÓN 
+print(f"Igualdad: 10 == 3 es {10 == 3} ")
+print(f"Desigualdad: 10 != 3 es {10 != 3} ")
+print(f"Mayor que: 10 > 3 es {10 > 3} ")
+print(f"Menor que: 10 < 3 es {10 < 3} ")
+print(f"Mayor o igual que: 10 >= 10 es {10 >= 10} ")
+print(f"Menor o igual que: 10 <= 3 es {10 <= 3} ")
+
+#operadores logicos
+print(f"AND &&: 10 + 3 == 13 and 5 - 1 == 4 es {10 + 3 == 13 and 5 - 1 == 4}")
+print(f"OR ||: 10 + 3 == 13 or 5 - 1 == 4 es {10 + 3 == 14 or 5 - 1 == 4}")
+print(f"NOT !: not 10 + 3 == 14 es {not 10 + 3 == 14}")
+
+#operadores de asignación
+my_number = 11
+print(my_number)
+my_number += 5  #suma y asignación 
+print(my_number)
+my_number -= 5  #resta y asignación
+print(my_number)
+my_number *= 5  #multiplicación y asignación
+print(my_number)
+my_number /= 5  #división y asignación
+print(my_number)
+my_number %= 5  #m+odulo y asignación
+print(my_number)
+my_number **= 5 #exponente y asignación
+print(my_number)
+my_number //= 5 #división entera y asignación
